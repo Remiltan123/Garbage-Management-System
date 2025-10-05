37b1ce887d93d7f581ad2f36bfa8a26c8baf99db
remiltan123 <regiremiltan2002@gmail.com>
2025-10-05 02:16:02 -0700

    Login page is added

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import { LoginPage } from "../LoginPage/LoginPage";
-import './HomePage.css'
-
+import './HomePage.css';
 
 export function HomePage() {
-    return (
-        <div className="home-page-bg">
-            <div className="div"> <LoginPage/> </div>
-        </div>
-    )
+  return (
+    <div className="home-page">
+      <div className="home-page-bg"></div>
+      <div className="login-container">
+        <LoginPage />
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,34 @@
+import './LoginPage.css'
+import awarenessImage from '../../assets/awareness_of_waste_recycling.png'
 
+export function LoginPage() {
+    return (
+        <div className="Login-page-bg">
+            <div className='awarenessImage-container'>
+                <img src={awarenessImage} alt="awarenessImage" />
+            </div>
+            <form className="login-page-container">
+                <div className='login-page-input'>
+                    <label className="" >UserName</label>
+                    <input type="text" className="" required placeholder='Name'/>
+                </div>
+                <div className='login-page-input'>
+                    <label className="login-page-input" >Email</label>
+                    <input type="email" className="" required placeholder='Email'/>
+                </div>
+                <div className='login-page-input'>
+                    <label className="login-page-input" >Password</label>
+                    <input type="password" className="" required placeholder='Password' />
+                </div >
+                <div>
+                    Don't have an account?<span> Sign up</span>
 
-export function LoginPage(){
-    return(
-        <>
-        <div className="p">Hoi</div>
-        </>
+                </div>
+                <button type="submit" className="">
+                    Login
+                </button>
+            </form>
+
+        </div>
     )
 }
