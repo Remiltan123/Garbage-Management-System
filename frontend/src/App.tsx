2b1a8865049dbc7d966440dedbac52d01db8fcf8
remiltan123 <regiremiltan2002@gmail.com>
2025-10-15 00:01:28 -0700

    remove the unwanted comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/images/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import { HomePage } from './pages/HomePage/HomePage'
-// import { BrowserRouter , Routes, Route } from 'react-router-dom'
-// import { DashBoard } from './Components/DashBoard/DashBoard'
-
-//  function App() {
-//   return (
-//    <BrowserRouter >
-//       <Routes>
-//         <Route path="/" element={<HomePage/>} />
-//         <Route path="/dashboard" element={<DashBoard/>} />
-//       </Routes >
-//     </BrowserRouter >
-//   )
-// }
-
-// export default App;
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DashBoard } from "./Components/DashBoard/DashBoard";
 import { WasteageClassifier } from "./Components/WasteageClassifier/WasteageClassifier";
